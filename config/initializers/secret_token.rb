07ee3d86b72a14487781805b78f33d6f04ec02bd
Keisuke Kondou <keisukehokkai0930@yahoo.co.jp>
2012-10-23 15:44:01 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catarogu::Application.config.secret_token = '07da7b6023a618b680950677f4bf7a49c3e1ae40c5d158bec59eaf2d0da009c702719755e2d0c62af906d82aed6e248c32cc336333db89fae956cb75b34147ed'
