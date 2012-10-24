f7e667167ec5cf740a95076be7a260d074b38caf
Keisuke Kondou <keisukehokkai0930@yahoo.co.jp>
2012-10-24 13:59:50 +0900

    [#38317595] 店舗を管理するページを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023080507) do
+ActiveRecord::Schema.define(:version => 20121024044541) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20121023080507) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "price",       :default => 0
+    t.string   "image"
+  end
+
+  create_table "shops", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "lines_summary"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
     t.string   "image"
   end
 
